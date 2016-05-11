db5e1568bc287fed2692cb19b220bcc2c9dee24e
victoria <victoriajko@users.noreply.github.com>
2016-05-10 19:55:03 -0700

    change font

--- server/app.py
+++ server/app.py
@@ -8,7 +8,7 @@ from .database import Database
 from .blueprints import root
 
 def create_app(config_object):
-    app = Flask(__name__, static_folder='../client/static')
+    app = Flask(__name__)
     app.config.from_object(config_object)
 
     # Connect to database
